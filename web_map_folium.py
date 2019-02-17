cd772baa738d46e5d40f8cbbf99a8b7179ee960d
miskoom <hmiskoom@yahoo.com>
2019-02-17 23:01:34 +0100

    included folium.plugin, changed tiles to CartoDB dark_matter

--- web_map_folium.py
+++ web_map_folium.py
@@ -1,7 +1,28 @@
 import folium
+from folium.plugins import MarkerCluster
+import pandas as pd
 
+
+# load data 
+data = pd.read_csv('Volcanoes_USA.txt')
+lat = data['LAT']
+lon = data['LON']
+elevation = data['ELEV']
+
+# Function to change colors of markers
+def color_change(elev):
+    if(elev < 1000):
+        return('green')
+    elif(1000 <= elev < 3000):
+        return('orange')
+    else:
+        return ('red')
 
 # Create base map
-map = folium.Map(location=[8.373988, 8.402566], zoom_start = 8)
+map = folium.Map(location=[37.296933,-121.9574983], zoom_start = 5, tiles = "CartoDB dark_matter")
+
+# multiple markers
+for lat, lon, elevation in zip(lat, lon, elevation):
+    folium.CircleMarker(location=[lat, lon], radius=9, popup=str(elevation)+" m", fill_color=color_change(elevation), color='gray', fill_opacity=0.9).add_to(map)
 
 map.save(outfile='map1.html')
